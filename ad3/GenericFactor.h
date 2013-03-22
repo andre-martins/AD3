1093847c3d2e6e0c6f5efe17d39ae911844864eb
Andre Martins <andre.t.martins@gmail.com>
2013-03-22 15:20:05 +0000

    Cleaned some whitespaces.

--- ad3/GenericFactor.h
+++ ad3/GenericFactor.h
@@ -94,14 +94,14 @@ class GenericFactor : public Factor {
                         Configuration &configuration,
                         double *value) = 0;
 
-  // Given a configuration with a probability (weight), 
+  // Given a configuration with a probability (weight),
   // increment the vectors of variable and additional posteriors.
   virtual void UpdateMarginalsFromConfiguration(
     const Configuration &configuration,
     double weight,
     vector<double> *variable_posteriors,
     vector<double> *additional_posteriors) = 0;
-    
+
   // Count how many common values two configurations have.
   virtual int CountCommonValues(
     const Configuration &configuration1,
