ecf1aeaf63b30469cd5f4dae158288a53fe5bbe9
Forest Gregg <fgregg@uchicago.edu>
2013-07-16 10:39:08 -0500

    build_clib must be called before build_ext
    Added __init__, readme, fixed build on 64 bit.
    
    Improve setup.py, add Manifest.in
    
    INSTALL remove spurious specialization, add fix for bdist_egg.

--- setup.py
+++ setup.py
@@ -1,45 +1,9 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.command.bdist_egg import bdist_egg
+#from distutils.command.build_clib import build_clib
+from setuptools.extension import Extension
 from Cython.Distutils import build_ext
-from distutils.extension import Extension
-from distutils.command.build_clib import build_clib
-from distutils.errors import DistutilsSetupError
-from distutils import log
 
-
-class build_libad3(build_clib):
-    def build_libraries(self, libraries):
-        for (lib_name, build_info) in libraries:
-            sources = build_info.get('sources')
-            if sources is None or not isinstance(sources, (list, tuple)):
-                raise DistutilsSetupError(
-                    "in 'libraries' option (library '%s'), " +
-                    "'sources' must be present and must be " +
-                    "a list of source filenames" % lib_name)
-            sources = list(sources)
-
-            log.info("building '%s' library", lib_name)
-
-            # First, compile the source code to object files in the library
-            # directory. (This should probably change to putting object
-            # files in a temporary build directory.)
-            macros = build_info.get('macros')
-            include_dirs = build_info.get('include_dirs')
-            extra_compile_args = build_info.get('extra_compile_args')
-            objects = self.compiler.compile(sources,
-                                            output_dir=self.build_temp,
-                                            macros=macros,
-                                            include_dirs=include_dirs,
-                                            extra_preargs=extra_compile_args,
-                                            debug=self.debug,
-                                            )
-
-            # Now "link" the object files together into a static library.
-            # (On Unix at least, this isn't really linking -- it just
-            # builds an archive. Whatever.)
-            self.compiler.create_static_lib(objects, lib_name,
-                                            output_dir=self.build_clib,
-                                            debug=self.debug)
 libad3 = ('ad3', {
     'sources': ['ad3/FactorGraph.cpp',
                 'ad3/GenericFactor.cpp',
@@ -59,15 +23,28 @@ libad3 = ('ad3', {
     ],
 })
 
+# this is a backport of a workaround for a problem in distutils.
+# install_lib doesn't call build_clib
+
+
+class bdist_egg_fix(bdist_egg):
+    def run(self):
+        self.call_command('build_clib')
+        bdist_egg.run(self)
+
 
 setup(name='ad3',
-      package_dir = {'ad3': 'python/ad3'},
-      packages = ['ad3'],
+      version="2.0",
+      author="Andre Martins",
+      url="http://www.ark.cs.cmu.edu/AD3",
+      author_email="afm@cs.cmu.edu",
+      package_dir={'ad3': 'python/ad3'},
+      packages=['ad3'],
       libraries=[libad3],
-      cmdclass={'build_clib': build_libad3, 'build_ext' : build_ext},
-      ext_modules=[Extension("ad3.factor_graph", 
-                             ["python/factor_graph.pyx"], 
-                             include_dirs = ["ad3"],
+      cmdclass={'build_ext': build_ext, 'bdist_egg': bdist_egg_fix},
+      #cmdclass={'build_ext': build_ext},
+      ext_modules=[Extension("ad3.factor_graph",
+                             ["python/factor_graph.pyx"],
+                             include_dirs=["ad3"],
                              language="c++",
                              )])
-
