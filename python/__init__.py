ed438cc9b53b2ab429c1c081255d1508617b5a15
Vlad Niculae <vlad@vene.ro>
2017-01-06 09:31:59 +0900

    add new solve() interface, remove xrange in simple_inference
    i imagine this is not too bad of a slowdown, under py2, to
    warrant bringing in six.moves.

--- python/__init__.py
+++ python/__init__.py
@@ -1,4 +1,4 @@
 __version__ = '2.1.dev0'
 
 from .factor_graph import PBinaryVariable, PFactorGraph, PMultiVariable
-from .simple_inference import simple_grid, general_graph
+from .simple_inference import solve, simple_grid, general_graph
