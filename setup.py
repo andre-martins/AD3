e223724d44bbb47463284a8641c1aef08041e4c0
Vlad Niculae <vlad@vene.ro>
2017-07-31 10:19:30 -0400

    freeze version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ if WHEELHOUSE_UPLOADER_COMMANDS.intersection(sys.argv):
 
 
 setup(name='ad3',
-      version="2.1.dev0",
+      version="2.1",
       author="Andre Martins",
       description='Alternating Directions Dual Decomposition',
       url="http://www.ark.cs.cmu.edu/AD3",
