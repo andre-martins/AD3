c99697687b6e14bcb6f2c83a96965a15df2f3bc6
Vlad Niculae <vlad@vene.ro>
2018-02-17 10:17:36 -0500

    version mismatch in init

--- python/__init__.py
+++ python/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.1.dev0'
+__version__ = '2.2.dev0'
 
 from .factor_graph import PBinaryVariable, PFactorGraph, PMultiVariable
 from .simple_inference import simple_grid, general_graph
