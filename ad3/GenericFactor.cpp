3e847ed2efa36adc1eb599d982dd45a2d694e8e8
Vlad Niculae <vlad@vene.ro>
2018-06-20 16:45:11 -0400

    Forgot to bump a few files. Release 2.2

--- ad3/GenericFactor.cpp
+++ ad3/GenericFactor.cpp
@@ -3,18 +3,18 @@
 //
 // This file is part of AD3 2.2.
 //
-// AD3 2.1 is free software: you can redistribute it and/or modify
+// AD3 2.2 is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// AD3 2.1 is distributed in the hope that it will be useful,
+// AD3 2.2 is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with AD3 2.1.  If not, see <http://www.gnu.org/licenses/>.
+// along with AD3 2.2.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "GenericFactor.h"
 #include "Utils.h"
